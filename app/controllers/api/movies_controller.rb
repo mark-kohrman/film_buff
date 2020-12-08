b1c1728e7331fcec2c6658585bdbd1dbc47c604b
Mark-Kohrman <mark.kohrman@gmail.com>
2020-12-07 20:25:27 -0600

    add show action for movies

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -3,4 +3,9 @@ class Api::MoviesController < ApplicationController
     @movies = Movie.all
     render 'index.json.jb'
   end
+
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 end
