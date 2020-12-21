d620a2665f1ff6fb99c4f5eb3926e04d1f1b2b70
Mark-Kohrman <mark.kohrman@gmail.com>
2020-12-21 11:48:33 -0600

    add img_url to searches via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_172844) do
+ActiveRecord::Schema.define(version: 2020_12_21_174632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_12_21_172844) do
     t.integer "thumbs_down"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img_url"
   end
 
 end
