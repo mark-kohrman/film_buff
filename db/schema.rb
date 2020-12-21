48fe2c6c3e580b609f1a5d6a5988b2e2f43334ea
Mark-Kohrman <mark.kohrman@gmail.com>
2020-12-21 09:35:59 -0600

    add thumbs_up as integer to movies via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_152106) do
+ActiveRecord::Schema.define(version: 2020_12_21_153431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_12_21_152106) do
     t.string "img_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "thumbs_up"
   end
 
 end
