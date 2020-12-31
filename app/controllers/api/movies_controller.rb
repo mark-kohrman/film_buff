122e5b7d8f3a97d31c538e9cd4bd7d1dca9c6aa1
Mark-Kohrman <mark.kohrman@gmail.com>
2020-12-30 18:31:38 -0600

    add db logic to movie show

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -10,7 +10,26 @@ class Api::MoviesController < ApplicationController
     response = HTTP.get("http://www.omdbapi.com/?apikey=#{Rails.application.credentials.movie_api[:api_key]}&i=#{imbd_id}")
     response.parse
     @movie = response.parse
-    
+    saved_movie = Movie.find_by(id: params[:id])
+    p saved_movie
+    p "SAVED*********"
+    if saved_movie
+      @movie = saved_movie
+    else
+      added_movie = Movie.create(
+        title: @movie["Title"],
+        director: @movie["Director"],
+        release_year: @movie["Year"],
+        runtime: @movie["Runtime"],
+        description: @movie["Plot"],
+        img_url: @movie["Poster"],
+        thumbs_up: 0,
+        thumbs_down: 0
+      )
+    end
+
+    p added_movie
+
     render 'show.json.jb'
   end
 end
